4a9a7a0c764be24d7db246a1b015114abd0fa2a9
Grayce Muthui <graycemuthui20@gmail.com>
2023-05-14 22:29:12 +0300

    Create signup

---
+++ src/authentication/Signup.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Signup = ({ setStoredToken }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("/api/v1/users", {
+      method: "POST",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          email,
+          password,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        console.log(data);
+        setStoredToken(data.token);
+      });
+
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="signup">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Signup;
